1437af9f2c0769ba258dfd26716ca6b6a4959544
liyang <356514033@qq.com>
2018-04-19 20:14:35 +0800

    update to es5

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 //get type
-const getType = (obj) => Object.prototype.toString.call(obj).slice(8, -1);
-
+function getType(obj) {
+  return Object.prototype.toString.call(obj).slice(8, -1);
+}
 //deep copy
-const deepCopy = (obj) => JSON.parse(JSON.stringify(obj));
-
+function deepCopy(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
 //
-const deleteArrayVal = (arrTobeDel, indexItem) => {
-  let arrtype = getType(arrTobeDel),
+function deleteArrayVal(arrTobeDel, indexItem) {
+  var arrtype = getType(arrTobeDel),
     indextype = getType(indexItem);
 
   if (arrtype != 'Array' || indextype != 'Array') throw new Error(`arguments is not an Array`);
 
   //索引集合里面必须全部为‘number’
-  for (let i = 0; i < indexItem.length; ++i) {
+  for (var i = 0; i < indexItem.length; ++i) {
     if (typeof indexItem[i] != 'number') throw new TypeError(`index is not a number`);
   }
 
   //避免后面的操作改变传入数组的值
-  let newArrItem = deepCopy(arrTobeDel),
+  var newArrItem = deepCopy(arrTobeDel),
     newIndexItem = deepCopy(indexItem);
-    //将传入的数组按照倒序排列
-    newIndexItem.sort((a, b) => b - a);
-    
-    for(let i = 0; i < newIndexItem.length; i++){
-      newArrItem.splice(newIndexItem[i], 1);
-    }
+  //将传入的数组按照倒序排列
+  newIndexItem.sort((a, b) => b - a);
+
+  for (var j = 0; j < newIndexItem.length; j++) {
+    newArrItem.splice(newIndexItem[j], 1);
+  }
 
   return newArrItem;
 }
